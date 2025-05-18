50591ba8465cd84c7a4e0313727ecd91687a0a6e
yaslavka <90184154+yaslavka@users.noreply.github.com>
2025-05-18 20:27:18 +0300

    ok

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
-    server.listen(80, () => console.log(`server started on port 5000`));
+    server.listen(5002, () => console.log(`server started on port 5000`));
     //httpsServer.listen(443, () => console.log(`server started on port 443`));
     const typeMatrixSecondCount = await models.TypeMatrixSecond.count()
     const typeMatrixThirdCount = await models.TypeMatrixThird.count()
